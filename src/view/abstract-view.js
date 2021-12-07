db949843dc5d63346f5413393b952ec9bb785a35
marteom <marteom@gmail.com>
2021-12-07 10:34:19 +0300

    fix lint errors

--- src/view/abstract-view.js
+++ src/view/abstract-view.js
@@ -1,29 +1,29 @@
-import {createElement} from '../helpers/render.js';
+import { createElement } from '../helpers/render.js';
 
 export default class AbstractView {
-    #element = null;
-    _callback = {};
+  #element = null;
+  _callback = {};
 
-    constructor() {
-      if (new.target === AbstractView) {
-        throw new Error('Can\'t instantiate AbstractView, only concrete one.');
-      }
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('Can\'t instantiate AbstractView, only concrete one.');
+    }
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    get element() {
-      if (!this.#element) {
-        this.#element = createElement(this.template);
-      }
+    return this.#element;
+  }
 
-      return this.#element;
-    }
+  get template() {
+    throw new Error('Abstract method not implemented: get template');
+  }
 
-    get template() {
-      throw new Error('Abstract method not implemented: get template');
-    }
-
-    removeElement() {
-      this.#element = null;
-    }
+  removeElement() {
+    this.#element = null;
+  }
 
 }
